36492fbf0064fc99c8f2213f393348551ca919ad
mdmuhibullahhabib <mdmuhibullahhabib6348@gmail.com>
2025-12-05 22:44:52 +0600

    home

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+    images: {
+    domains: [
+      'images.unsplash.com',
+      "i.ibb.co"
+    ],
+  },
   reactCompiler: true,
 };
 
